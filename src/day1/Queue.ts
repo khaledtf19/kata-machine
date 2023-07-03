09cf1da72ca1b1ae2bae496f1995cb6df100c1e7
Khaledtf Labeb <khaledtf19@gmail.com>
2023-07-03 18:15:39 +0300

    Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,12 +1,46 @@
+type Node<T> = {
+  vlaue: T;
+  next?: Node<T>;
+};
 export default class Queue<T> {
   public length: number;
-  private head: T;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
   constructor() {
-    length = 0;
+    this.length = 0;
+    this.head = this.tail = undefined;
   }
 
-  enqueue(item: T): void {}
-  deque(): T | undefined {}
-  peek(): T | undefined {}
+  enqueue(item: T): void {
+    let newNode = { vlaue: item } as Node<T>;
+    if (!this.tail) {
+      this.head = this.tail = newNode;
+    } else {
+      const oldTail = this.tail;
+      oldTail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
+    let newhead = this.head.next;
+    let oldHead = this.head;
+    if (!newhead) {
+      this.head = this.tail = newhead;
+      this.length--;
+      return oldHead.vlaue;
+    }
+    this.head = newhead;
+    this.length--;
+    return oldHead?.vlaue;
+  }
+
+  peek(): T | undefined {
+    return this.head?.vlaue;
+  }
 }
