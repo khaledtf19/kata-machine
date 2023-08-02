929aaf78fcc63bbc7274cf31b549122449009c83
Khaledtf Labeb <khaledtf19@gmail.com>
2023-08-02 21:55:10 +0300

    maze 1

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,66 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+const dir = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+];
 
+function walk(
+  maze: string[],
+  curr: Point,
+  wall: string,
+  end: Point,
+  path: Point[],
+  seen: boolean[][],
+): boolean {
+  if (
+    curr.x < 0 ||
+    curr.x >= maze[0].length ||
+    curr.y < 0 ||
+    curr.y >= maze.length
+  ) {
+    return false;
+  }
+
+  if (maze[curr.y][curr.x] === wall) {
+    return false;
+  }
+
+  if (curr.y === end.y && curr.x === end.x) {
+    path.push(end);
+    return true;
+  }
+
+  if (seen[curr.y][curr.x]) {
+    return false;
+  }
+
+  seen[curr.y][curr.x] = true;
+  path.push(curr);
+
+  for (let i = 0; i < dir.length; i++) {
+    const [x, y] = dir[i];
+    if (walk(maze, { x: curr.x + x, y: curr.y + y }, wall, end, path, seen)) {
+      return true;
+    }
+  }
+
+  path.pop();
+  return false;
+}
+
+export default function solve(
+  maze: string[],
+  wall: string,
+  start: Point,
+  end: Point,
+): Point[] {
+  const path = [] as Point[];
+  const seen = [] as boolean[][];
+
+  for (let i = 0; i < maze.length; i++) {
+    seen.push(new Array(maze[0].length).fill(false));
+  }
+  walk(maze, start, wall, end, path, seen);
+  return path;
 }
