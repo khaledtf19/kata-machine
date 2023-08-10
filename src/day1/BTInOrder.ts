23ea597a84ef2dc1d35daa82ed75589265b1aba0
Khaledtf Labeb <khaledtf19@gmail.com>
2023-08-10 22:04:32 +0300

    QuickSort, BT-All

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,15 @@
 export default function in_order_search(head: BinaryNode<number>): number[] {
+  const arr: number[] = [];
+  search(arr, head);
+  return arr;
+}
 
+function search(arr: number[], curr: BinaryNode<number>) {
+  if (curr.left) {
+    search(arr, curr.left);
+  }
+  arr.push(curr.value);
+  if (curr.right) {
+    search(arr, curr.right);
+  }
 }
--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,15 @@
 export default function post_order_search(head: BinaryNode<number>): number[] {
-
+  const arr: number[] = [];
+  search(arr, head);
+  return arr;
+}
+function search(arr: number[], curr: BinaryNode<number>) {
+  if (curr.left) {
+    search(arr, curr.left);
+  }
+  if (curr.right) {
+    search(arr, curr.right);
+  }
+  arr.push(curr.value);
+  return;
 }
